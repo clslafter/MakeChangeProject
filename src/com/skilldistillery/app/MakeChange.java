b1396293c69302f9dbdabb6f68212ee1dd9e8922
clslafter <clslafter@att.net>
2022-08-19 17:14:28 -0500

    user story 1: user promt for price

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -1,10 +1,19 @@
 package com.skilldistillery.app;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.println("Please enter the price of the item");
+		double price = kb.nextDouble();
+		
+		
+		
 	}
 
 }
