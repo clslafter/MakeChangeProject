6e665d78308608439324efa4e2bcbbb155a2796e
clslafter <clslafter@att.net>
2022-08-20 11:08:15 -0500

    change through one dollar bills

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -8,33 +8,33 @@ public class MakeChange {
 		// TODO Auto-generated method stub
 
 		Scanner kb = new Scanner(System.in);
-		
+
 		double price = price();
-		
+
 		double amount = amount();
-		
+
 		lessOrExact(price, amount);
-		
+
 		kb.close();
-		
+
 	}
 
 	public static double price() {
 		Scanner kb = new Scanner(System.in);
-		
+
 		System.out.println("Please enter the price of the item");
 		double price = kb.nextDouble();
 		return price;
 	}
-	
+
 	public static double amount() {
 		Scanner kb = new Scanner(System.in);
-		
+
 		System.out.println("Please enter the amount provided by the customer");
 		double amount = kb.nextDouble();
 		return amount;
 	}
-	
+
 	public static void lessOrExact(double price, double amount) {
 		if (amount == price) {
 			System.out.println("Exact amount. No change due");
@@ -50,44 +50,49 @@ public class MakeChange {
 		// TODO Auto-generated method stub
 		double changeAmount = amount - price;
 		System.out.printf("Change due: $%.2f. Result: ", changeAmount);
-		
-		double numTwenties = changeAmount / 20;		
-		
-		if (numTwenties > 0) {
-			if ((int) numTwenties > 1) {
-				System.out.print( (int) numTwenties + " twenty dollar bills, ");
-			} else if ((int) numTwenties == 1){
-				System.out.print((int) numTwenties + " twenty dollar bill, ");
-			}
+
+		double numTwenties = changeAmount / 20;
+
+		if ((int) numTwenties > 1) {
+			System.out.print((int) numTwenties + " twenty dollar bills, ");
+		} else if ((int) numTwenties == 1) {
+			System.out.print((int) numTwenties + " twenty dollar bill, ");
 		}
-		
+
 		changeAmount = changeAmount - (20.00 * (int) numTwenties);
 //		System.out.printf("$%.2f", changeAmount);
-		
-		double numTens = changeAmount / 10;		
-		
-//		if (numTens > 0) {
-			if ((int) numTens > 1) {
-				System.out.print( (int) numTens + " ten dollar bills, ");
-			} else if ((int) numTens == 1) {
-				System.out.println((int) numTens + " ten dollar bill, ");
-			}
-//		} 
-		
+
+		double numTens = changeAmount / 10;
+
+		if ((int) numTens > 1) {
+			System.out.print((int) numTens + " ten dollar bills, ");
+		} else if ((int) numTens == 1) {
+			System.out.print((int) numTens + " ten dollar bill, ");
+		}
+
 		changeAmount = changeAmount - (10.00 * (int) numTens);
 //		System.out.printf("$%.2f", changeAmount);
-		
-		double numFives = changeAmount / 5;		
-		
-//		if (numFives > 0) {
-			if ((int) numFives > 1) {
-				System.out.print( (int) numFives + " five dollar bills, ");
-			} else if ((int) numFives == 1) {
-				System.out.println((int) numFives + " five dollar bill, ");
-			}
-//		} 
-		
-		changeAmount = changeAmount - (5.00 * (int) numTens);
+
+		double numFives = changeAmount / 5;
+
+		if ((int) numFives > 1) {
+			System.out.print((int) numFives + " five dollar bills, ");
+		} else if ((int) numFives == 1) {
+			System.out.print((int) numFives + " five dollar bill, ");
+		}
+
+		changeAmount = changeAmount - (5.00 * (int) numFives);
 //		System.out.printf("$%.2f", changeAmount);
+
+		double numOnes = changeAmount / 1;
+//
+		if ((int) numOnes > 1) {
+			System.out.print((int) numOnes + " one dollar bills, ");
+		} else if ((int) numOnes == 1) {
+			System.out.print((int) numOnes + " one dollar bill, ");
+		}
+
+		changeAmount = changeAmount - (1.00 * (int) numOnes);
+		 System.out.printf("$%.2f", changeAmount);
 	}
 }
