651eeee6529ed77da7e8176ba131b1659e4334a3
clslafter <clslafter@att.net>
2022-08-21 11:18:44 -0500

    change working. no commas or periods

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -13,7 +13,7 @@ public class MakeChange {
 
 		double amount = amount();
 
-		lessOrExact(price, amount);
+		assessAmount(price, amount);
 
 		kb.close();
 
@@ -35,7 +35,7 @@ public class MakeChange {
 		return amount;
 	}
 
-	public static void lessOrExact(double price, double amount) {
+	public static void assessAmount(double price, double amount) {
 		if (amount == price) {
 			System.out.println("Exact amount. No change due");
 		} else if (amount < price) {
@@ -49,50 +49,99 @@ public class MakeChange {
 	private static void makeChange(double price, double amount) {
 		// TODO Auto-generated method stub
 		double changeAmount = amount - price;
-		System.out.printf("Change due: $%.2f. Result: ", changeAmount);
+		System.out.printf("Change due: $%.2f. Return: ", changeAmount);
 
 		double numTwenties = changeAmount / 20;
 
 		if ((int) numTwenties > 1) {
-			System.out.print((int) numTwenties + " twenty dollar bills, ");
+			System.out.print((int) numTwenties + " twenty dollar bills ");
 		} else if ((int) numTwenties == 1) {
-			System.out.print((int) numTwenties + " twenty dollar bill, ");
+			System.out.print((int) numTwenties + " twenty dollar bill ");
 		}
+		
 
 		changeAmount = changeAmount - (20.00 * (int) numTwenties);
-//		System.out.printf("$%.2f", changeAmount);
-
+//		System.out.printf("\n $%.2f", changeAmount);
+		
 		double numTens = changeAmount / 10;
 
 		if ((int) numTens > 1) {
-			System.out.print((int) numTens + " ten dollar bills, ");
+			System.out.print((int) numTens + " ten dollar bills ");
 		} else if ((int) numTens == 1) {
-			System.out.print((int) numTens + " ten dollar bill, ");
+			System.out.print((int) numTens + " ten dollar bill ");
 		}
 
 		changeAmount = changeAmount - (10.00 * (int) numTens);
-//		System.out.printf("$%.2f", changeAmount);
+//		System.out.printf("\n $%.2f", changeAmount);
 
 		double numFives = changeAmount / 5;
 
 		if ((int) numFives > 1) {
-			System.out.print((int) numFives + " five dollar bills, ");
+			System.out.print((int) numFives + " five dollar bills ");
 		} else if ((int) numFives == 1) {
-			System.out.print((int) numFives + " five dollar bill, ");
+			System.out.print((int) numFives + " five dollar bill ");
 		}
 
 		changeAmount = changeAmount - (5.00 * (int) numFives);
-//		System.out.printf("$%.2f", changeAmount);
+//		System.out.printf("\n $%.2f", changeAmount);
 
 		double numOnes = changeAmount / 1;
-//
+
 		if ((int) numOnes > 1) {
-			System.out.print((int) numOnes + " one dollar bills, ");
+			System.out.print((int) numOnes + " one dollar bills ");
 		} else if ((int) numOnes == 1) {
-			System.out.print((int) numOnes + " one dollar bill, ");
+			System.out.print((int) numOnes + " one dollar bill ");
 		}
 
 		changeAmount = changeAmount - (1.00 * (int) numOnes);
-		 System.out.printf("$%.2f", changeAmount);
+//		 System.out.printf("\n $%.2f", changeAmount);
+		 
+		 double numQuarters = changeAmount / 0.25;
+
+			if ((int) numQuarters > 1) {
+				System.out.print ((int) numQuarters + " quarters ");
+			} else if ((int) numQuarters == 1) {
+				System.out.print((int) numQuarters + " quarter ");
+			}
+
+			changeAmount = changeAmount - (0.25 * (int) numQuarters);
+//			 System.out.printf("\n $%.2f", changeAmount);
+			
+			double numDimes = changeAmount / 0.10 + 0.00000000000001;
+
+			if ((int) numDimes > 1) {
+				System.out.print ((int) numDimes + " dimes ");
+			} else if ((int) numDimes == 1) {
+				System.out.print((int) numDimes + " dime ");
+			}
+
+			changeAmount = changeAmount - (0.10 * (int) numDimes);
+//			 System.out.printf("\n $%.2f", changeAmount);
+			 
+			 double numNickels = (changeAmount / 0.05) + 0.001;
+//			 System.out.println("\n" + numNickels);
+
+				if ((int) numNickels > 1) {
+					System.out.print ((int) numNickels + " nickels ");
+				} else if ((int) numNickels == 1) {
+					System.out.print((int) numNickels + " nickel ");
+				}
+				
+				
+
+				changeAmount = changeAmount - (0.05 * (int) numNickels);
+//				 System.out.printf("\n $%.2f", changeAmount);
+				 
+				 double numPennies = (changeAmount / 0.01) + 0.0001;
+//				 System.out.println("\n" + numPennies);
+
+					if ((int) numPennies > 1) {
+						System.out.print ((int) numPennies + " pennies. ");
+					} else if ((int) numPennies == 1) {
+						System.out.print((int) numPennies + " penny. ");
+					}
+
+					changeAmount = changeAmount - (0.01 * (int) numPennies);
+//					 System.out.printf("\n $%.2f", changeAmount);
 	}
 }
